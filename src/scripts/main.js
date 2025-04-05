879ebb71876818812280b94eb19e8fae51e9d509
dimon <80713na@gmail.com>
2025-04-06 01:28:50 +0300

    spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const wallWidth = wall.offsetWidth;
+const wallHeight = wall.offsetHeight;
+
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
+
+const newTop = (wallHeight - spiderHeight) / 2;
+const newLeft = (wallWidth - spiderWidth) / 2;
+
+spider.style.left = `${newLeft}px`;
+spider.style.top = `${newTop}px`;
