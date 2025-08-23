f47d4afb0f151fd2f852f4d7ce8e6535130dc53f
dimon <80713na@gmail.com>
2025-08-23 13:24:25 +0300

    +_+

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,7 @@
 'use strict';
 
-const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-const wallWidth = wall.offsetWidth;
-const wallHeight = wall.offsetHeight;
-
-const spiderWidth = spider.offsetWidth;
-const spiderHeight = spider.offsetHeight;
-
-const newTop = (wallHeight - spiderHeight) / 2;
-const newLeft = (wallWidth - spiderWidth) / 2;
-
-spider.style.left = `${newLeft}px`;
-spider.style.top = `${newTop}px`;
+spider.style.top = '50%';
+spider.style.left = '50%';
+spider.style.transform = 'translate(-50%, -50%)';
